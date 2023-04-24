81f19d1986531d99036089a1fd7e59a1015caf00
Dexter <djalquisola@gmail.com>
2023-04-24 19:35:03 +0800

    sendnotif-function

--- src/utils/firebase-notification.js
+++ src/utils/firebase-notification.js
@@ -1,9 +1,36 @@
 import admin from "firebase-admin";
+import fcm  from "fcm-notification";
+import serviceAccount from "../../lft_secret_token.json";
 
-// import serviceAccount from "path/to/serviceAccountKey.json";
+var certPath = admin.credential.cert(serviceAccount);
+var FCM = new fcm(certPath);
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+function sendPushNotification(firebaseToken, title, messageBody) {
+  try {
+    let message = {
+      android: {
+        ttl: 3600 * 1000, // 1 hour in milliseconds
+        priority: "high",
+        notification: {
+          title: title,
+          body: messageBody,
+        },
+      },
+      token: firebaseToken,
+    };
 
-export default admin;
+    FCM.send(message, function (err, response) {
+      if (err) {
+        console.log("Something has gone wrong!");
+        //res.send(err);
+      } else {
+        //console.log("Successfully sent with response: ", response);
+        //res.send(response);
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export default sendPushNotification;
